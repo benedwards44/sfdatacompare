3db88a3e43a4ebe7ab551f781bc08ca078575037
Ben Edwards <bedwards@tquila.com>
2015-10-01 13:19:48 +0100

    Stuff

--- sfdatacompare/urls.py
+++ sfdatacompare/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from comparedata import views
 
 urlpatterns = [
-	url(r'^$', 'comparedata.views.index', name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
