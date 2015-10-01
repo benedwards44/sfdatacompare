d3b702f3446c8acfe44a4405a4cfa728074eaa5f
Ben Edwards <bedwards@tquila.com>
2015-10-01 13:36:38 +0100

    Stuff

--- comparedata/views.py
+++ comparedata/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import View
+from django.conf import settings
 
 import sys
 
@@ -16,4 +17,8 @@ class IndexView(View):
 
 	# When view is called via GET
 	def get(self, request, *args, **kwargs):
-		return render(request, self.template_name, {})
+		
+		return render(request, self.template_name, {
+			'client_id': settings.SALESFORCE_CONSUMER_KEY,
+			'redirect_uri': settings.SALESFORCE_REDIRECT_URI
+		})
