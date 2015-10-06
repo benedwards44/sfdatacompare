411bb02142a354454f69cfa6845ad4b9e060deb0
Ben Edwards <bedwards@tquila.com>
2015-10-06 19:16:44 +0100

    Stuff

--- comparedata/admin.py
+++ comparedata/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from comparedata.models import Job, Org, Object, ObjectField
 
-# Register your models here.
+class OrgInline(admin.TabularInline):
+	fields = ['org_number','org_name', 'username', 'access_token', 'status', 'error']
+	ordering = ['org_number',]
+	model = Org
+	extra = 0
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('created_date','finished_date','status','error')
+    ordering = ['-created_date']
+    inlines = [OrgInline,]
+
+
+admin.site.register(Job, JobAdmin)
