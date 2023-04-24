8a7f5fa34516a5e98bf139c46d8f6eda4347facb
Ben Edwards <ben@edwards.nz>
2023-04-24 14:59:37 +1200

    Remove ignored files

--- sfdatacompare/wsgi.py
+++ sfdatacompare/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for sfdatacompare project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sfdatacompare.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sfdatacompare.settings')
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
